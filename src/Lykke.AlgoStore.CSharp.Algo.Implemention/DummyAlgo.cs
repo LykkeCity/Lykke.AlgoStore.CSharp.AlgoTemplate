ce6d435f860b58dcb1e4b71cef6ef6e03ab767b0
Miladin Joksic <miladin.joksic@primeholding.com>
2018-01-25 16:11:47 +0100

    Added Algo core and implementation projects

---
+++ src/Lykke.AlgoStore.CSharp.Algo.Implemention/DummyAlgo.cs
@@ -0,0 +1,16 @@
+﻿using Lykke.AlgoStore.CSharp.Algo.Core.Domain;
+
+namespace Lykke.AlgoStore.CSharp.Algo.Implemention
+{
+    /// <summary>
+    /// REMARK: Just a dummy algo implementation for future reference.
+    /// We can and will remove this when first algo is implemented :)
+    /// </summary>
+    public class DummyAlgo : IAlgo
+    {
+        public void OnQuoteReceived(IContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
