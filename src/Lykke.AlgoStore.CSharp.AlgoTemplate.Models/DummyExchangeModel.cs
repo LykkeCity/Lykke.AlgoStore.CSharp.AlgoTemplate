948ef2283ef6cb5369f0bd09e0b3715ad8fd404c
Miladin Joksic <miladin.joksic@primeholding.com>
2018-01-29 15:54:08 +0100

    Added new project that will be used to exchange model data between APIs (as nuget package)

---
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Models/DummyExchangeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Models
+{
+    /// <summary>
+    /// REMARK: Just a dummy exchange model for future reference.
+    /// We can and will remove this when first exchange model is needed :)
+    /// </summary>
+    public class DummyExchangeModel
+    {
+    }
+}
