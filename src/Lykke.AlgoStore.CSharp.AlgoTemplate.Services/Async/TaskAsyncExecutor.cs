484910cb8b3edd1df1c940d827a2d973f0991400
atanas-arnaudov-prime <atanas.arnaudov@primeholding.com>
2018-02-01 16:12:12 +0200

    Adding abstraction to execute asynchronous Action

---
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Services/Async/TaskAsyncExecutor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Async
+{
+    /// <summary>
+    /// <see cref="IAsyncExecutor"/> implementation using <see cref="Task"/>
+    /// to achieve asynchronous execution
+    /// </summary>
+    public class TaskAsyncExecutor : IAsyncExecutor
+    {
+        public void Execute(Action a) => Task.Run(a);
+
+        public void ExecuteAsync<T>(Action<T> a, T param) => Task.Run(() => a(param));
+    }
+}
