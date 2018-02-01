8d9820b89e0ec99df365a001ead4e34456bc22dd
atanas-arnaudov-prime <atanas.arnaudov@primeholding.com>
2018-02-01 14:28:21 +0200

    Adding the IAlgoQuote interface implementation

---
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Core/Domain/AlgoQuote.cs
@@ -0,0 +1,21 @@
+﻿using Lykke.AlgoStore.CSharp.Algo.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Domain
+{
+    /// <summary>
+    /// <see cref="IAlgoQuote"/> implementation
+    /// </summary>
+    public class AlgoQuote : IAlgoQuote
+    {
+        public bool IsBuy { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public bool IsOnline { get; set; }
+    }
+}
