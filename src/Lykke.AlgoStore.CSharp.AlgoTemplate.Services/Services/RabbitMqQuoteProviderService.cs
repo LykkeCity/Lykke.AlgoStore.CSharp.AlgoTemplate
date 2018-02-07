53b3da0a94bbc221bcad7e2c5466fb8f90edf434
Plamen Ralovski <ralovski>
2018-02-07 14:04:54 +0200

    starting of implementation of RabbitMqQuoteProviderService

---
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Services/Services/RabbitMqQuoteProviderService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Lykke.AlgoStore.CSharp.Algo.Core.Domain;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Services;
+
+namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Services
+{
+    public class RabbitMqQuoteProviderService : IQuoteProviderService
+    {
+        public Task Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe(Action<IAlgoQuote> action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
