8b6b43f0b4ad48a7fa683f709c1f4a54daf6bd59
atanas-arnaudov-prime <atanas.arnaudov@primeholding.com>
2018-02-12 14:04:39 +0200

    Adding a service for working with Candles
    The interface does not have an implementation yet - the only implementation provided is for feeding with a random data.

---
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Core/Domain/CandleService/MultipleCandlesResponse.cs
@@ -0,0 +1,14 @@
+﻿using Lykke.AlgoStore.CSharp.Algo.Core.Candles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Domain.CandleService
+{
+    public class MultipleCandlesResponse
+    {
+        public string RequestId { get; set; }
+
+        public IList<Candle> Candles { get; set; }
+    }
+}
