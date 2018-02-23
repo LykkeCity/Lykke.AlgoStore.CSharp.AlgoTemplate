56fc0a47ca8195327562b1875721af7eecea8fc4
dafinka.gerdjikova <dafinka.gerdjikova@programista.pro>
2018-02-23 14:50:49 +0200

    Add Additional Unit Tests

--- tests/Lykke.AlgoStore.CSharp.AlgoTemplate.Tests/Unit/AdxFunctionTests.cs
+++ tests/Lykke.AlgoStore.CSharp.AlgoTemplate.Tests/Unit/AdxFunctionTests.cs
@@ -218,6 +218,33 @@ namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Tests.Unit
         }
 
         [Test]
+        public void CalculateDMIPlusFunctionWarmUpAndAddTheRest()
+        {
+            var function = new DirectionalMovemnetIndexPlusFunction(new DMIParameters() { Priod = DEFAULT_PERCISION });
+            var values = GetTestCandles("Adx_Examples2.txt").ToList();
+
+            var valuesToWarmUp = values.GetRange(0, 15);
+            var valuesToAdd = values.Skip(15).ToList();
+
+            double? dmiPlus = 0.0d;
+            double valueFirstDMIPlusCheck = 25.20;
+            double valueFinalDMIPlusToCheck = 19.26;
+
+            dmiPlus = function.WarmUp(valuesToWarmUp);
+
+            Assert.NotNull(dmiPlus);
+            Assert.AreEqual(valueFirstDMIPlusCheck, Math.Round(dmiPlus.Value, 2));
+
+            foreach (var val in valuesToAdd)
+            {
+                dmiPlus = function.AddNewValue(val);
+            }
+
+            Assert.NotNull(dmiPlus);
+            Assert.AreEqual(valueFinalDMIPlusToCheck, Math.Round(dmiPlus.Value, 2));
+        }
+
+        [Test]
         public void CalculateDMIMinusFunction()
         {
             var function = new DirectionalMovemnetIndexMinusFunction(new DMIParameters() { Priod = DEFAULT_PERCISION });
@@ -256,10 +283,37 @@ namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Tests.Unit
             var function = new DirectionalMovemnetIndexMinusFunction(new DMIParameters() { Priod = DEFAULT_PERCISION });
             var values = GetTestCandles(_fileNameNotFullData);
 
-            double? dmiPlues = 0.0d;
+            double? dmiMinus = 0.0d;
 
-            dmiPlues = function.WarmUp(values);
-            Assert.IsNull(dmiPlues);
+            dmiMinus = function.WarmUp(values);
+            Assert.IsNull(dmiMinus);
+        }
+
+        [Test]
+        public void CalculateDMIMinusFunctionWarmUpAndAddTheRest()
+        {
+            var function = new DirectionalMovemnetIndexMinusFunction(new DMIParameters() { Priod = DEFAULT_PERCISION });
+            var values = GetTestCandles("Adx_Examples2.txt").ToList();
+
+            var valuesToWarmUp = values.GetRange(0, 15);
+            var valuesToAdd = values.Skip(15).ToList();
+
+            double? dmiMinus = 0.0d;
+            double valueFirstDMIMinusCheck = 17.13;
+            double valueFinalDMIMinusToCheck = 29.47;
+
+            dmiMinus = function.WarmUp(valuesToWarmUp);
+
+            Assert.NotNull(dmiMinus);
+            Assert.AreEqual(valueFirstDMIMinusCheck, Math.Round(dmiMinus.Value, 2));
+
+            foreach (var val in valuesToAdd)
+            {
+                dmiMinus = function.AddNewValue(val);
+            }
+
+            Assert.NotNull(dmiMinus);
+            Assert.AreEqual(valueFinalDMIMinusToCheck, Math.Round(dmiMinus.Value, 2));
         }
     }
 }
