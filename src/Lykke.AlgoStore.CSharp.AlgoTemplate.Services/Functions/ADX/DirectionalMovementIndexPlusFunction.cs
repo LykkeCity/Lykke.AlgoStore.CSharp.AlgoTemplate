61ddd9167f57929cc4f9ee2de3757ad0885e90b1
dafinka.gerdjikova <dafinka.gerdjikova@programista.pro>
2018-02-26 12:10:56 +0200

    Raname Separate functions for directional indexes

--- src/Lykke.AlgoStore.CSharp.AlgoTemplate.Services/Functions/ADX/DirectionalMovementIndexPlusFunction.cs
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Services/Functions/ADX/DirectionalMovementIndexPlusFunction.cs
@@ -3,7 +3,6 @@ using Lykke.AlgoStore.CSharp.Algo.Core.Functions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Functions.ADX
 {
