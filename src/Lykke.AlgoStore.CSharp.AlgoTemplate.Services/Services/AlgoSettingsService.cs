c124aaf166c04c5fd4e8564ce917ef746e77b952
Miladin Joksic <miladin.joksic@primeholding.com>
2018-02-27 15:24:50 +0100

    Added basic settings service implementation with tests

--- src/Lykke.AlgoStore.CSharp.AlgoTemplate.Services/Services/AlgoSettingsService.cs
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Services/Services/AlgoSettingsService.cs
@@ -1,7 +1,9 @@
 ﻿using Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Services;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Services
 {
@@ -10,14 +12,26 @@ namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Services
     /// </summary>
     public class AlgoSettingsService : IAlgoSettingsService
     {
+        private string _settingsJson;
+        private IDictionary<string, object> _settings;
+
         public void Initialize()
         {
+            _settingsJson = Environment.GetEnvironmentVariable("ALGO_INSTANCE_PARAMS");
 
+            if (String.IsNullOrEmpty(_settingsJson))
+                throw new ArgumentException("Environment variable 'ALGO_INSTANCE_PARAMS' does not contain settings");
+
+            dynamic dynamicSettings = JsonConvert.DeserializeObject<ExpandoObject>(_settingsJson);
+            _settings = (IDictionary<string, object>) dynamicSettings;
         }
 
         public string GetSetting(string key)
         {
-            throw new NotImplementedException();
+            if (!_settings.ContainsKey(key))
+                return string.Empty;
+
+            return _settings[key] as string;
         }
 
     }
