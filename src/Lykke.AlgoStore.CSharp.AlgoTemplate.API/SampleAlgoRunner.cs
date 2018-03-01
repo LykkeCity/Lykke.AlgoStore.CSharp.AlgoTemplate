13e82e1cef2db99c02b60ce5d41a81826ea047aa
dafinkagerdjikova <dafinka.gerdjikova@primeholding.com>
2018-03-01 15:03:27 +0200

    Remove MAC parameters form functions solution

--- src/Lykke.AlgoStore.CSharp.AlgoTemplate.API/SampleAlgoRunner.cs
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.API/SampleAlgoRunner.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using Common.Log;
 using Lykke.AlgoStore.CSharp.Algo.Core.Domain;
 using Lykke.AlgoStore.CSharp.Algo.Implemention;
+using Lykke.AlgoStore.CSharp.Algo.Implemention.MovingAverageCross;
 using Lykke.AlgoStore.CSharp.AlgoTemplate.AzureRepositories.Mapper;
 using Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Services;
 using Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Settings;
@@ -29,7 +30,7 @@ namespace Lykke.AlgoStore.CSharp.AlgoTemplate
             // Initialize eagerly the class for the algo assembly so
             // that the runtime loads the algos assembly prior to
             // tunning the main
-            DEFAULT_ALGO_CLASS_TO_RUN = typeof(MacdFunction);
+            DEFAULT_ALGO_CLASS_TO_RUN = typeof(MacdTrendAlgo);
         }
 
         public static async Task Main(string[] args)
