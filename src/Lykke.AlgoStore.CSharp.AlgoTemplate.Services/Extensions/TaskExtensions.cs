508cfe0ed902e97debdda3005f1012fb6abd7f6b
Filip Lyubenov <filip.lyubenov@primeholding.com>
2018-06-21 16:51:17 +0300

    Replace ContinueWith with TaskDelay.  Add private constructor and allow only TradeResponses created by the factory methods.

--- src/Lykke.AlgoStore.CSharp.AlgoTemplate.Services/Extensions/TaskExtensions.cs
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Services/Extensions/TaskExtensions.cs
@@ -1,16 +1,19 @@
 ﻿using Lykke.AlgoStore.CSharp.AlgoTemplate.Core;
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Extensions
 {
     public static class TaskExtensions
     {
-        public static async Task<T> WithCancellation<T>(this Task<T> task)
+        public static async Task<T> WithTimeout<T>(this Task<T> targetTask)
         {
-            var token = new CancellationTokenSource(TimeSpan.FromSeconds(Constants.ServiceCallTimoutSeconds)).Token;
-            return await task.ContinueWith(t => t.GetAwaiter().GetResult(), token);
+            var timeout = Task.Delay(TimeSpan.FromSeconds(Constants.ServiceCallTimeoutSeconds));
+            var taskWhichFinishedFirst = await Task.WhenAny(targetTask, timeout);
+            if (taskWhichFinishedFirst != targetTask)
+                throw new TaskCanceledException(targetTask);
+
+            return await targetTask;
         }
     }
 }
