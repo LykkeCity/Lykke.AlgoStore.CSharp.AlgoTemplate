71af865dc775105e3766cc6f8b5e1f3351d588a1
Miladin Joksic <miladin.joksic@primeholding.com>
2018-06-22 10:59:50 +0200

    Added logging service client injection

--- src/Lykke.AlgoStore.CSharp.AlgoTemplate.Core/Settings/AppSettings.cs
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Core/Settings/AppSettings.cs
@@ -1,5 +1,6 @@
 ﻿using Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Settings.ServiceSettings;
 using Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Settings.SlackNotifications;
+using Lykke.AlgoStore.Service.Logging.Client;
 
 namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Settings
 {
@@ -9,5 +10,6 @@ namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Settings
         public SlackNotificationsSettings SlackNotifications { get; set; }
         public MatchingEngineAdapterSettings MatchingEngineAdapterClient { get; set; }
         public CandlesHistoryServiceClient CandlesHistoryServiceClient { get; set; }
+        public LoggingServiceClientSettings AlgoStoreLoggingServiceClient { get; set; }
     }
 }
