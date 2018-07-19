76ceef7781b3684c85206a03edeec038d7e91e31
Filip Lyubenov <filip.lyubenov@primeholding.com>
2018-07-19 12:59:28 +0300

    ChartingUpdate contracts added

---
+++ src/Lykke.AlgoStore.Algo/Charting/CandleChartingUpdate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.AlgoStore.Algo.Charting
+{
+    public class CandleChartingUpdate : Candle, IChartingUpdate
+    {
+        public string InstanceId { get; set; }
+    }
+}
---
+++ src/Lykke.AlgoStore.Algo/Charting/FunctionChartingUpdate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.AlgoStore.Algo.Charting
+{
+    public class FunctionChartingUpdate : IChartingUpdate
+    {
+        public string FunctionName { get; set; }
+        public double Value { get; set; }
+        public string InstanceId { get; set; }
+        public List<FunctionChartingUpdate> InnerFunctions { get; set; }
+    }
+}
