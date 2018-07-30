8975e12be1953f9aee8b834688bd8f152ba3c395
Iliyan Georgiev <iliyan.georgiev@primeholding.com>
2018-07-30 16:29:42 +0300

    Create generic BatchSubmitter class

--- src/Lykke.AlgoStore.CSharp.AlgoTemplate.Services/Services/UserLogService.cs
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Services/Services/UserLogService.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using JetBrains.Annotations;
 using Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Services;
-using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Repositories;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Utils;
 using Lykke.AlgoStore.Service.Logging.Client;
 using Lykke.Service.Logging.Client.AutorestClient.Models;
 
@@ -13,15 +13,10 @@ namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Services
     /// <summary>
     /// <see cref="IUserLogService"/> implementation
     /// </summary>
-    public class UserLogService : IUserLogService, IDisposable
+    public class UserLogService : IUserLogService
     {
         [NotNull] private readonly ILoggingClient _userLogClient;
-        [NotNull] private readonly BatchBlock<UserLogRequest> _batchBlock;
-        [NotNull] private readonly ActionBlock<UserLogRequest[]> _actionBlock;
-
-        private readonly TimeSpan _maxBatchLifetime;
-        private DateTime _currentBatchExpirationMoment;
-        private readonly Timer _timer;
+        [NotNull] private readonly BatchSubmitter<UserLogRequest> _batchSubmitter;
 
         private readonly IAlgoSettingsService _algoSettingsService;
         private string _authToken;
@@ -31,35 +26,13 @@ namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Services
             int batchSizeThreshold,
             IAlgoSettingsService algoSettingsService)
         {
-            if (maxBatchLifetime < TimeSpan.Zero)
-            {
-                throw new ArgumentOutOfRangeException(nameof(maxBatchLifetime), maxBatchLifetime,
-                    "Should be positive time span");
-            }
-
-            if (batchSizeThreshold < 1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(batchSizeThreshold), batchSizeThreshold,
-                    "Should be positive number");
-            }
-
-            _maxBatchLifetime = maxBatchLifetime;
             _userLogClient = userLogClient;
             _algoSettingsService = algoSettingsService;
 
-            _batchBlock = new BatchBlock<UserLogRequest>(batchSizeThreshold);
-
-            _actionBlock = new ActionBlock<UserLogRequest[]>(x => PersistUserLogs(x),
-                new ExecutionDataflowBlockOptions {MaxDegreeOfParallelism = 2});
-
-            _batchBlock.LinkTo(_actionBlock);
-
             SetAuthToken();
 
-            ExtendBatchExpiration();
-
-            //Initiate timer after 1s (1000ms) with a period of 50ms
-            _timer = new Timer(HandleTimerTriggered, null, 1000, 50);
+            _batchSubmitter = new BatchSubmitter<UserLogRequest>(
+                maxBatchLifetime, batchSizeThreshold, PersistUserLogs);
         }
 
         private void SetAuthToken()
@@ -69,12 +42,12 @@ namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Services
 
         public void Enqueue(UserLogRequest userLog)
         {
-            _batchBlock.Post(userLog);
+            _batchSubmitter.Enqueue(userLog);
         }
 
         public void Enqueue(string instanceId, string message)
         {
-            _batchBlock.Post(new UserLogRequest
+            _batchSubmitter.Enqueue(new UserLogRequest
             {
                 InstanceId = instanceId,
                 Date = DateTime.UtcNow,
@@ -82,35 +55,9 @@ namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Services
             });
         }
 
-        private Task PersistUserLogs(UserLogRequest[] userLogRequests)
+        private async Task PersistUserLogs(UserLogRequest[] userLogRequests)
         {
-            return _userLogClient.WriteAsync(userLogRequests, _authToken);
-        }
-
-        private void HandleTimerTriggered(object state)
-        {
-            if (DateTime.UtcNow > _currentBatchExpirationMoment)
-            {
-                _batchBlock.TriggerBatch();
-
-                ExtendBatchExpiration();
-            }
-        }
-
-        private void ExtendBatchExpiration()
-        {
-            _currentBatchExpirationMoment = DateTime.UtcNow + _maxBatchLifetime;
-        }
-
-        public void Dispose()
-        {
-            _batchBlock.Complete();
-            _batchBlock.Completion.ConfigureAwait(false).GetAwaiter().GetResult();
-
-            _actionBlock.Complete();
-            _actionBlock.Completion.ConfigureAwait(false).GetAwaiter().GetResult();
-
-            _timer.Dispose();
+            await _userLogClient.WriteAsync(userLogRequests, _authToken);
         }
     }
 }
---
+++ tests/Lykke.AlgoStore.CSharp.AlgoTemplate.Tests/Unit/BatchSubmitterTests.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Services;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Repositories;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Services;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Services.Utils;
+using Lykke.AlgoStore.Service.Logging.Client;
+using Lykke.Service.Logging.Client.AutorestClient.Models;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Tests.Unit
+{
+    [TestFixture]
+    public class BatchSubmitterTests
+    {
+        private readonly Fixture _fixture = new Fixture();
+
+        [Test]
+        public void Batch_Persistance_Is_Not_Triggered_If_There_Are_No_Entries()
+        {
+            Task failure(object[] items)
+            {
+                Assert.Fail();
+                return Task.CompletedTask;
+            }
+
+            using (var submitter = new BatchSubmitter<object>(TimeSpan.FromMilliseconds(100), 1, failure))
+            {
+                Task.Delay(200).Wait();
+            }
+        }
+
+        [Test]
+        public void Batch_Persistance_Is_Not_Triggered_If_Entries_Amount_Not_Reached_And_Batch_Lifetime_Is_Expired()
+        {
+            var shouldSucceed = false;
+
+            Task failure(object[] items)
+            {
+                Assert.IsTrue(shouldSucceed);
+                return Task.CompletedTask;
+            }
+
+            using (var submitter = new BatchSubmitter<object>(TimeSpan.FromHours(1), 100, failure))
+            {
+                var data = _fixture.CreateMany<object>(90);
+
+                foreach (var obj in data)
+                {
+                    submitter.Enqueue(obj);
+                }
+
+                Task.Delay(50);
+
+                shouldSucceed = true;
+            }
+        }
+
+        [Test]
+        public void Batch_Persistance_Is_Triggered_By_Entries_Amount()
+        {
+            var shouldSucceed = false;
+
+            Task failure(object[] items)
+            {
+                Assert.AreEqual(100, items.Length);
+                shouldSucceed = true;
+                return Task.CompletedTask;
+            }
+
+            using (var submitter = new BatchSubmitter<object>(TimeSpan.FromHours(1), 100, failure))
+            {
+                var data = _fixture.CreateMany<object>(100);
+
+                foreach (var obj in data)
+                {
+                    submitter.Enqueue(obj);
+                }
+
+                Task.Delay(50).Wait();
+
+                Assert.IsTrue(shouldSucceed);
+            }
+        }
+
+        [Test]
+        public void Batch_Persistance_Is_Triggered_By_Time_Expiration()
+        {
+            var shouldSucceed = false;
+
+            Task write(DateTime[] times)
+            {
+                Assert.AreEqual(1, times.Length);
+
+                foreach (var time in times)
+                {
+                    Assert.That(DateTimeOffset.UtcNow - time, Is.InRange(TimeSpan.FromSeconds(1), TimeSpan.FromMilliseconds(1300)));
+                }
+
+                shouldSucceed = true;
+                return Task.CompletedTask;
+            }
+
+            using (var submitter = new BatchSubmitter<DateTime>(TimeSpan.FromSeconds(1), 100, write))
+            {
+                submitter.Enqueue(DateTime.UtcNow);
+
+                Task.Delay(TimeSpan.FromSeconds(2)).Wait();
+            }
+
+            Assert.IsTrue(shouldSucceed);
+        }
+
+        [Test]
+        public void Batch_Persistance_Is_Triggered_By_Disposing()
+        {
+            var shouldSucceed = false;
+
+            Task write(object[] data)
+            {
+                Assert.AreEqual(1, data.Length);
+
+                shouldSucceed = true;
+                return Task.CompletedTask;
+            }
+
+            using (var submitter = new BatchSubmitter<object>(TimeSpan.FromHours(1), 10, write))
+            {
+                submitter.Enqueue(new object());
+            }
+
+            Assert.IsTrue(shouldSucceed);
+        }
+
+        [Test]
+        public void Batch_Persistence_Is_Triggered_By_Entries_Amount_Extends_Batch_Lifetime()
+        {
+            var batchTimes = new List<DateTime>();
+            var batchCounts = new List<int>();
+
+            Task write(object[] items)
+            {
+                batchTimes.Add(DateTime.UtcNow);
+                batchCounts.Add(items.Length);
+
+                return Task.CompletedTask;
+            }
+
+            using (var submitter = new BatchSubmitter<object>(TimeSpan.FromSeconds(1), 10, write))
+            {
+                var data = _fixture.CreateMany<UserLogRequest>(9);
+
+                foreach (var userLogRequest in data)
+                {
+                    submitter.Enqueue(userLogRequest);
+                }
+
+                //Wait half of a batch lifetime
+                Task.Delay(500).Wait();
+
+                //Complete batch
+                submitter.Enqueue(new UserLogRequest());
+
+                //Perform a batch
+                Task.Delay(50).Wait();
+
+                //Fill new batch
+                submitter.Enqueue(new UserLogRequest());
+
+                //Let batch time elapse
+                Task.Delay(2000).Wait();
+
+                Assert.AreEqual(2, batchTimes.Count);
+                Assert.AreEqual(10, batchCounts.First());
+                Assert.AreEqual(1, batchCounts.Last());
+            }
+        }
+    }
+}
