415696a66e3e01ca233d0d5068f162e01c8c4fd7
Iliyan Georgiev <iliyan.georgiev@primeholding.com>
2018-07-31 10:28:04 +0300

    Implement event collector service

---
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Core/Services/IEventCollector.cs
@@ -0,0 +1,12 @@
+﻿using Lykke.AlgoStore.Algo.Charting;
+using System.Threading.Tasks;
+
+namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Core.Services
+{
+    public interface IEventCollector
+    {
+        Task SubmitTradeEvent(TradeChartingUpdate trade);
+        Task SubmitCandleEvent(CandleChartingUpdate candle);
+        Task SubmitFunctionEvent(FunctionChartingUpdate function);
+    }
+}
