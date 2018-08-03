d6d9c09483910cb570886f08083d3cf3ab0337d6
Filip Lyubenov <filip.lyubenov@primeholding.com>
2018-08-03 16:26:23 +0300

    FunctionChartingUpdateRepository added

---
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Models/Models/FunctionChartingUpdateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Models
+{
+    public class FunctionChartingUpdateData
+    {
+        public string FunctionName { get; set; }
+        public double Value { get; set; }
+        public string InstanceId { get; set; }
+        public DateTime CalculatedOn { get; set; }
+        public List<FunctionChartingUpdateData> InnerFunctions { get; set; }
+    }
+}
