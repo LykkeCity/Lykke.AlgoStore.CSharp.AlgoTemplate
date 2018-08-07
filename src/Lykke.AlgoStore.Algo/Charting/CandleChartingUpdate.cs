6be4223c5b2d8e3ae52eb482022712a444f7467a
Iliyan Georgiev <iliyan.georgiev@primeholding.com>
2018-08-07 09:31:40 +0300

    Add AssetPair and CandleTimeInterval to CandleChartingUpdate

--- src/Lykke.AlgoStore.Algo/Charting/CandleChartingUpdate.cs
+++ src/Lykke.AlgoStore.Algo/Charting/CandleChartingUpdate.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Enumerators;
 
 namespace Lykke.AlgoStore.Algo.Charting
 {
     public class CandleChartingUpdate : Candle, IChartingUpdate
     {
         public string InstanceId { get; set; }
+
+        public string AssetPair { get; set; }
+
+        public CandleTimeInterval CandleTimeInterval { get; set; }
     }
 }
