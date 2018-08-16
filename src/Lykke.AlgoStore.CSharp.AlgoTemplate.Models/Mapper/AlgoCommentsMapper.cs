20f63d440999aa1a53c7b32176cd697e5dd6332d
Iliyan Georgiev <iliyan.georgiev@primeholding.com>
2018-08-16 10:28:09 +0300

    Move algo and comments repositories to models

---
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Models/Mapper/AlgoCommentsMapper.cs
@@ -0,0 +1,67 @@
+﻿using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Entities;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Mapper
+{
+    public static class AlgoCommentsMapper
+    {
+        public static AlgoCommentData ToModel(this AlgoCommentEntity entity)
+        {
+            var result = new AlgoCommentData
+            {
+                AlgoId = entity.PartitionKey,
+                CommentId = entity.RowKey,
+                Author = entity.AuthorId,
+                Content = entity.Content,
+                CreatedOn = entity.CreatedOn,
+                EditedOn = entity.EditedOn
+            };
+
+            return result;
+        }
+
+        public static List<AlgoCommentData> ToModel(this List<AlgoCommentEntity> entities)
+        {
+            var result = new List<AlgoCommentData>();
+
+            foreach (var entity in entities)
+            {
+                var data = new AlgoCommentData
+                {
+                    AlgoId = entity.PartitionKey,
+                    CommentId = entity.RowKey,
+                    Author = entity.AuthorId,
+                    Content = entity.Content,
+                    CreatedOn = entity.CreatedOn,
+                    EditedOn = entity.EditedOn
+                };
+
+                result.Add(data);
+            }            
+
+            return result;
+        }
+
+        public static AlgoCommentEntity ToEntity(this AlgoCommentData data)
+        {
+            var result = new AlgoCommentEntity
+            {
+                PartitionKey = data.AlgoId,
+                RowKey = data.CommentId,
+                AuthorId = data.Author,
+                Content = data.Content,
+                CreatedOn = data.CreatedOn,
+                EditedOn = data.EditedOn
+            };
+
+            return result;
+        }
+
+        public static List<AlgoCommentEntity> ToEntity(this List<AlgoCommentData> entities)
+        {
+            return entities.Select(e => e.ToEntity()).ToList();
+        }
+    }
+}
---
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Models/Models/AlgoDataInformation.cs
@@ -0,0 +1,24 @@
+﻿using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Enumerators;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Models.AlgoMetaDataModels;
+using System;
+
+namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Models
+{
+    public class AlgoDataInformation
+    {
+        public string AlgoId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public DateTime? DatePublished { get; set; }
+        public AlgoVisibility AlgoVisibility { get; set; }
+        public string Author { get; set; }
+
+        public double Rating { get; set; }
+        public int RatedUsersCount { get; set; }
+        public int UsersCount { get; set; }
+
+        public AlgoMetaDataInformation AlgoMetaDataInformation { get; set; }
+    }
+}
