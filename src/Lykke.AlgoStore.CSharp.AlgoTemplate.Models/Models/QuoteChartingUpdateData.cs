40643166612505cdab520532426718c2f557bef6
Filip Lyubenov <filip.lyubenov@primeholding.com>
2018-08-16 14:39:54 +0300

    Add QuoteChartingUpdateRepository

---
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Models/Models/QuoteChartingUpdateData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Models
+{
+    public class QuoteChartingUpdateData
+    {
+        public string InstanceId { get; set; }
+        public string AssetPair { get; set; }
+        public bool IsBuy { get; }
+        public double Price { get; }
+        public DateTime Timestamp { get; }
+        public DateTime DateReceived { get; }
+        public bool IsOnline { get; }
+    }
+}
