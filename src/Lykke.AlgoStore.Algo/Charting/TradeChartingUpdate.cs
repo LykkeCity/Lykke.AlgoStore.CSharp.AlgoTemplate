40cdd92e10d1a53707f81f6f02b86f6c2def89df
Iliyan Georgiev <iliyan.georgiev@primeholding.com>
2018-08-21 17:26:25 +0300

    Make TradeChartingUpdate inherit from IAlgoInstanceTrade

--- src/Lykke.AlgoStore.Algo/Charting/TradeChartingUpdate.cs
+++ src/Lykke.AlgoStore.Algo/Charting/TradeChartingUpdate.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Models;
+﻿using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Models;
+using System;
 
 namespace Lykke.AlgoStore.Algo.Charting
 {
-    public class TradeChartingUpdate : AlgoInstanceTrade, IChartingUpdate
+    public class TradeChartingUpdate : IAlgoInstanceTrade, IChartingUpdate
     {
-        public new string InstanceId { get; set; }
+        public string InstanceId { get; set; }
+        public string Id { get; set; }
+        public bool? IsBuy { get; set; }
+        public double? Price { get; set; }
+        public double? Amount { get; set; }
+        public double? Fee { get; set; }
+        public string OrderId { get; set; }
+        public string AssetPairId { get; set; }
+        public DateTime? DateOfTrade { get; set; }
+        public string AssetId { get; set; }
     }
 }
