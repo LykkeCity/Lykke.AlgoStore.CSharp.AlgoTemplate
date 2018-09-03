a7ec8be5201fde54f76e7f923c8454b366aae3ae
Miladin Joksic <miladin.joksic@primeholding.com>
2018-09-03 10:33:55 +0200

    Updated trade entity and model to have order type

---
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Models/Enumerators/OrderType.cs
@@ -0,0 +1,10 @@
+﻿namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Enumerators
+{
+    public enum OrderType
+    {
+        Unknown = 0,
+        Market = 1,
+        Limit = 2,
+        Stop = 3
+    }
+}
