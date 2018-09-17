35b87bc049ab4dd9b8b04c77c30c914ebcab82a5
Filip Lyubenov <filip.lyubenov@primeholding.com>
2018-09-17 15:56:49 +0300

    Add CreateOrUpdateAlgoInstanceOrderAsync to repo

--- src/Lykke.AlgoStore.CSharp.AlgoTemplate.Models/Repositories/AlgoInstanceTradeRepository.cs
+++ src/Lykke.AlgoStore.CSharp.AlgoTemplate.Models/Repositories/AlgoInstanceTradeRepository.cs
@@ -106,24 +106,6 @@ namespace Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Repositories
             await _tableStorage.InsertOrMergeAsync(entity);
         }
 
-        public async Task CreateOrUpdateAlgoInstanceOrderAsync(AlgoInstanceTrade data)
-        {
-            var entity = AutoMapper.Mapper.Map<AlgoInstanceTradeEntity>(data);
-            entity.PartitionKey = GeneratePartitionKeyByOrderId(data.OrderId);
-            entity.RowKey = GenerateRowKeyByWalletId(data.WalletId);
-
-            await _tableStorage.InsertOrMergeAsync(entity);
-        }
-
-        public async Task CreateOrUpdateAlgoInstanceOrderAsync(AlgoInstanceTrade data)
-        {
-            var entity = AutoMapper.Mapper.Map<AlgoInstanceTradeEntity>(data);
-            entity.PartitionKey = GeneratePartitionKeyByOrderId(data.OrderId);
-            entity.RowKey = GenerateRowKeyByWalletId(data.WalletId);
-
-            await _tableStorage.InsertOrMergeAsync(entity);
-        }
-
         /// <summary>
         /// Create new trade row with PK - instance Id and RK - ReverseTick
         /// </summary>
